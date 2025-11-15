9521e6ae18cfbe066c2d928da6201e5dbed2c097
alokumar01 <alokkumar012148@gmail.com>
2025-11-15 22:10:55 +0530

    Implement solution for Leetcode Question No 239 - Sliding Window Maximum

---
+++ Leetcode_Solution/leetcode239.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
+        vector<int> res;
+
+        for (int i = 0; i < k; i++) {
+            pq.push(make_pair(nums[i], i));
+        }
+        res.push_back(pq.top().first);
+
+        for (int i = k; i < nums.size(); i++) {
+            while (!pq.empty() && pq.top().second <= (i - k)) {
+                pq.pop();
+            }
+            pq.push(make_pair(nums[i], i));
+            res.push_back(pq.top().first);
+        }
+
+        return res;
+    }
+};
+
+[ Time taken: 15 m 35 s ] REVISION
+TC -O(n log k)
+SC - O(K)
+
+We can use deque to make this optimal
